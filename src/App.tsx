eeb44cf824fa137785daaf3ce562f1b9ddc4d3e6
wb-hbc831417 <wb-hbc831417@alibaba-inc.com>
2021-12-29 16:28:35 +0800

    feat: 可以实现list了

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import { Form, Input, Select, Button } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import RenderForm from './easyForm';
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+};
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 20, offset: 4 },
+  },
+};
 
 function App() {
   const [form] = Form.useForm();
@@ -12,31 +30,97 @@ function App() {
     <RenderForm
       form={form}
       onFinish={onFinish}
+      {...formItemLayoutWithOutLabel}
       items={[
+        // {
+        //   component: <Input />,
+        //   name: 'name',
+        //   label: '姓名'
+        // },
+        // {
+        //   component: <Select options={[{ value: 11, label: 11 }, { value: 9, label: 9 }]} />,
+        //   name: 'age',
+        //   label: '年龄'
+        // },
+        // {
+        //   component: <Input />,
+        //   name: 'test',
+        //   label: 'test',
+        //   dependencies: ['name'],
+        //   rules: [
+        //     ({ getFieldValue }) => ({
+        //       validator: (_, value) => {
+        //         if (!value || getFieldValue('name') === value) {
+        //           return Promise.resolve();
+        //         }
+        //         return Promise.reject(new Error('姓名不一致'));
+        //       }
+        //     })
+        //   ]
+        // },
         {
-          component: <Input />,
-          name: 'name',
-          label: '姓名'
-        },
-        {
-          component: <Select options={[{ value: 11, label: 11 }, { value: 9, label: 9 }]} />,
-          name: 'age',
-          label: '年龄'
-        },
-        {
-          component: <Input />,
-          name: 'test',
-          label: 'test',
-          dependencies: ['name'],
+          name: 'names',
           rules: [
-            ({ getFieldValue }) => ({
-              validator: (_, value) => {
-                if (!value || getFieldValue('name') === value) {
-                  return Promise.resolve();
+            {
+              validator: async (_, names) => {
+                if (!names || names.length < 2) {
+                  return Promise.reject(new Error('At least 2 passengers'));
                 }
-                return Promise.reject(new Error('姓名不一致'));
-              }
-            })
+              },
+            },
+          ],
+          list: (fields, { add, remove }, { errors }) => [
+            ...fields.map((field, index) => ({
+              ...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel),
+              label: index === 0 ? 'Passengers' : '',
+              required: false,
+              component: [
+                {
+                  ...field,
+                  rules: [
+                    {
+                      required: true,
+                      whitespace: true,
+                      message: "Please input passenger's name or delete this field.",
+                    },
+                  ],
+                  noStyle: true,
+                  component: <Input style={{ width: '60%' }} />,
+                },
+                {
+                  noStyle: true,
+                  component: fields.length > 1 ? (
+                    <MinusCircleOutlined
+                      className="dynamic-delete-button"
+                      onClick={() => remove(field.name)}
+                    />
+                  ) : null
+                }
+              ]
+            })),
+            {
+              component: <>
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  style={{ width: '60%' }}
+                  icon={<PlusOutlined />}
+                >
+                  Add field
+                </Button>
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add('The head item', 0);
+                  }}
+                  style={{ width: '60%', marginTop: '20px' }}
+                  icon={<PlusOutlined />}
+                >
+                  Add field at head
+                </Button>
+                <Form.ErrorList errors={errors} />
+              </>
+            },
           ]
         },
         {
--- src/easyForm/renderForm.tsx
+++ src/easyForm/renderForm.tsx
@@ -1,10 +1,109 @@
 import React from 'react';
-import { Form, FormItemProps, FormInstance } from 'antd';
+import { Form, FormItemProps, FormInstance, } from 'antd';
+import { FormListProps } from 'antd/lib/form';
+import { FormListFieldData, FormListOperation } from 'antd/lib/form/FormList';
+
+
+type ObjComponetItem = FormItemProps & { component?: React.ReactElement | null, noWrapper?: boolean }
+type FuncComponentItem = FormItemProps & { component?: (props: FormInstance) => React.ReactElement }
+type ListComponentItem = FormItemProps & { component?: Array<ObjComponetItem | FuncComponentItem> }
+type EasyFormItem = ObjComponetItem | FuncComponentItem | ListComponentItem
+
+type EasyFormList = Omit<FormListProps, 'children'> & {
+  list: (fields: FormListFieldData[], operation: FormListOperation, meta: {
+    errors: React.ReactNode[];
+    warnings: React.ReactNode[];
+  }) => Array<EasyFormItem>,
+}
 
 interface EasyFormProps extends React.ComponentProps<typeof Form> {
-  items: Array<FormItemProps & {
-    component: React.ReactElement | ((form: FormInstance) => React.ReactElement)
-  }>
+  items: Array<EasyFormItem | EasyFormList>
+}
+
+/**
+ * 如果component为一个对象
+ */
+const renderNormalComponent = (props: ObjComponetItem) => {
+  const { component, noWrapper, ...restProps } = props;
+  if (noWrapper) {
+    return component
+  }
+  return (
+    <Form.Item {...restProps}>
+      {component}
+    </Form.Item>
+  )
+}
+
+/**
+ * 如果component为一个函数
+ */
+const renderFuncComponent = (props: FuncComponentItem) => {
+  const { component, dependencies, shouldUpdate, ...restItemProps } = props;
+  if (typeof component === 'function') {
+    return (
+      <Form.Item
+        noStyle
+        dependencies={dependencies}
+        shouldUpdate={shouldUpdate}
+      >
+        {(props) => (
+          <Form.Item {...restItemProps}>
+            {(component as Function)(props)}
+          </Form.Item>
+        )}
+      </Form.Item>
+    )
+  }
+}
+
+/**
+ * 如果component为list
+ */
+const renderListComponent = (props: ListComponentItem) => {
+  const { component, ...restProps } = props;
+  return (
+    <Form.Item {...restProps}>
+      {
+        component?.map(item => {
+          if (typeof item.component === 'function') {
+            return renderFuncComponent(item as FuncComponentItem);
+          } else {
+            return renderNormalComponent(item as ObjComponetItem);
+          }
+        })
+      }
+    </Form.Item>
+  )
+}
+
+/**
+ * 渲染Form.Item
+ */
+const renderObjItem = (item: EasyFormItem) => {
+  const { component } = item;
+  if (typeof component === 'function') {
+    return renderFuncComponent(item as FuncComponentItem);
+  }
+  if (Array.isArray(component)) {
+    return renderListComponent(item as ListComponentItem);
+  }
+  return renderNormalComponent(item as ObjComponetItem);
+}
+
+/**
+ * 如果item.type == 'list'，则渲染成Form.List
+ */
+const renderListItem = (item: EasyFormList) => {
+  const { list, ...restProps } = item;
+  return (
+    <Form.List {...restProps}>
+      {(...arg) => {
+        const itemList = list(...arg);
+        return itemList.map(renderObjItem);
+      }}
+    </Form.List>
+  )
 }
 
 const RenderForm: React.FC<EasyFormProps> = (props) => {
@@ -14,33 +113,12 @@ const RenderForm: React.FC<EasyFormProps> = (props) => {
   return (
     <Form {...restFormProps}>
       {
-        items.map(itemProps => {
-          const { component, dependencies, shouldUpdate, ...restItemProps } = itemProps;
-
-          if (typeof component === 'function') {
-            return (
-              <Form.Item
-                noStyle
-                dependencies={dependencies}
-                shouldUpdate={shouldUpdate}
-              >
-                {(props) => (
-                  <Form.Item {...restItemProps}>
-                    {(component as Function)(props)}
-                  </Form.Item>
-                )}
-              </Form.Item>
-            )
+        items.map(item => {
+          if ('list' in item) {
+            return renderListItem(item);
+          } else {
+            return renderObjItem(item);
           }
-          return (
-            <Form.Item
-              dependencies={dependencies}
-              shouldUpdate={shouldUpdate}
-              {...restItemProps}
-            >
-              {component}
-            </Form.Item>
-          )
         })
       }
     </Form>
